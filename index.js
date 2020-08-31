fb405e6610fcc6a0177884ab9d38798a00fafe90
jane muthoni <muchirijane@outlook.com>
2020-08-31 13:40:26 +0300

    removing github token

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const fs = require('fs');
 
 try {
     // `who-to-greet` input defined in action metadata file
-    const githubToken = core.getInput('github-token');
     const jsonData = fs.readFileSync('json-data').join("");
     const data = JSON.parse(jsonData);
     console.log(data);
