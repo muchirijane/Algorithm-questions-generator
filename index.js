926886d495d16309436ee3f36698bdf104c54e4d
jane muthoni <muchirijane@outlook.com>
2020-08-31 12:08:10 +0300

    added github token

--- index.js
+++ index.js
@@ -6,15 +6,10 @@ const fs = require('fs');
 try {
     // `who-to-greet` input defined in action metadata file
     const jsonData = fs.readFileSync('json-data');
-    const data = JSON.parse(jsonData);
+    const data = JSON.parse(jsonData).join("");
     console.log(data);
 
 
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
 } catch (error) {
     core.setFailed(error.message);
 }
